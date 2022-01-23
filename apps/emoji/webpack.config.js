5cab5a7e95e4af15897b09b6a3d121def01de16a
Wanchao Zhang <wonderbeyond@gmail.com>
2022-01-23 16:12:26 +0800

    misc updates

--- apps/emoji/webpack.config.js
+++ apps/emoji/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: 'auto',
   },
   module: {
     rules: [
